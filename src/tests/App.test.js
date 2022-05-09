2b7dd4e55572328d7ce866c27825f342e24919b4
Andre Rousselet <andre098@hotmail.com>
2022-05-09 20:02:18 -0300

    Altera renderWithRouterr de localização

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { screen } from '@testing-library/react';
 import App from '../App';
-import renderWithRouter from '../services/renderWithRouter';
+import renderWithRouter from './renderWithRouter';
 
 describe('Requisito 1:', () => {
   test('se o topo da aplicação contém um conjunto fixo de links de navegação.', () => {
