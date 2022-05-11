bac93e6584dd4de6ec13615106e5cd92328b42cf
Andre Rousselet <andre098@hotmail.com>
2022-05-11 15:01:18 -0300

    finaliza req 7

--- src/tests/PokemonDetails.test.js
+++ src/tests/PokemonDetails.test.js
@@ -3,6 +3,7 @@ import { screen } from '@testing-library/react';
 import renderWithRouter from './renderWithRouter';
 import App from '../App';
 import pokemons from '../data';
+import userEvent from '@testing-library/user-event';
 
 describe('Requisito 7: componente <PokemonDetails.js />', () => {
   test('se as informações detalhadas do pokémon selecionado são mostradas na tela',
@@ -40,12 +41,45 @@ describe('Requisito 7: componente <PokemonDetails.js />', () => {
         const imgLocations = screen.getAllByRole('img',
           { name: `${pokemon.name} location` });
         expect(imgLocations.length).toEqual(pokemon.foundAt.length);
+
         imgLocations.forEach((img, index) => {
           expect(img.src).toBe(pokemon.foundAt[index].map);
           const locationName = screen.getByText(pokemon.foundAt[index].location);
-          console.log(pokemon.foundAt[index].location);
           expect(locationName).toBeInTheDocument();
         });
       });
     });
+
+  test('se o usuário pode favoritar um pokémon através da página de detalhes.', () => {
+    const { history } = renderWithRouter(<App />);
+    pokemons.forEach((pokemon) => {
+      history.push(`/pokemons/${pokemon.id}`);
+
+      const checkbox = screen.getByRole('checkbox', { name: /pokémon favoritado\?/i });
+      expect(checkbox).toBeInTheDocument();
+      userEvent.click(checkbox);
+    });
+
+    history.push('/favorites');
+
+    pokemons.forEach((pokemon) => {
+      const pokemonImg = screen.getByRole('img', { name: `${pokemon.name} sprite` });
+      const favoriteStar = screen.getByRole('img',
+        { name: `${pokemon.name} is marked as favorite` });
+      expect(pokemonImg).toBeInTheDocument();
+      expect(favoriteStar).toBeInTheDocument();
+    });
+
+    pokemons.forEach((pokemon) => {
+      history.push(`/pokemons/${pokemon.id}`);
+
+      const checkbox = screen.getByRole('checkbox', { name: /pokémon favoritado\?/i });
+      userEvent.click(checkbox);
+    });
+
+    history.push('/favorites');
+
+    const notFoundText = screen.getByText(/no favorite pokemon found/i);
+    expect(notFoundText).toBeInTheDocument();
+  });
 });
