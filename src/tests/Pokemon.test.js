1b35cccf238609c9be4c743b92de5174a48fde64
Andre Rousselet <andre098@hotmail.com>
2022-11-13 14:31:12 -0300

    test(Pokemon): req 6: parte 3

--- src/tests/Pokemon.test.js
+++ src/tests/Pokemon.test.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { screen } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 import renderWithRouter from './renderWithRouter';
+import App from '../App';
 import Pokemon from '../components/Pokemon';
 import pokemons from '../data';
 
@@ -40,12 +41,27 @@ describe('Requisito 6: componente <Pokemon.js />', () => {
           <Pokemon pokemon={ pokemon } isFavorite={ false } />,
         );
 
-        const links = screen.getAllByRole('link', { name: /more details/i });
-        expect(links[index]).toBeInTheDocument();
-        expect(links[index]).toHaveAttribute('href', `/pokemons/${pokemon.id}`);
+        const detailsLink = screen.getAllByRole('link', { name: /more details/i });
+        expect(detailsLink[index]).toBeInTheDocument();
+        expect(detailsLink[index]).toHaveAttribute('href', `/pokemons/${pokemon.id}`);
 
-        userEvent.click(links[index]);
+        userEvent.click(detailsLink[index]);
         expect(history.location.pathname).toBe(`/pokemons/${pokemon.id}`);
       });
     });
+
+  test('Deve existir um ícone de estrela nos pokémons favoritados.', () => {
+    pokemons.forEach((pokemon) => {
+      renderWithRouter(<Pokemon pokemon={ pokemon } isFavorite />);
+
+      const favStar = screen.getByRole(
+        'img', { name: `${pokemon.name} is marked as favorite` },
+      );
+      expect(favStar).toBeInTheDocument();
+      expect(favStar).toHaveAttribute('src', '/star-icon.svg');
+      expect(favStar).toHaveAttribute(
+        'alt', `${pokemon.name} is marked as favorite`,
+      );
+    });
+  });
 });
